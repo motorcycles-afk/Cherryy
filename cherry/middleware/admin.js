2044c7bb4183d645f609f0b09f634503e8fb2254
motorcycles-afk <catttaa347@gmail.com>
2025-01-31 08:53:03 +0100

    Delete cherry directory

--- cherry/middleware/admin.js
+++
@@ -1,8 +0,0 @@
-const admin = (req, res, next) => {
-    if (!req.user.isAdmin) {
-        return res.status(403).json({ error: 'Access denied. Admin privileges required.' });
-    }
-    next();
-};
-
-export default admin;
