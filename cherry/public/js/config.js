1a479c3ad17516cba2bb0155eed4813de579c87f
motorcycles-afk <catttaa347@gmail.com>
2025-01-31 08:50:13 +0100

    Delete cherry directory

--- cherry/public/js/config.js
+++
@@ -1,8 +0,0 @@
-// API Configuration
-const config = {
-    apiBaseUrl: window.location.hostname === 'localhost' 
-        ? 'http://localhost:3000/api'
-        : '/api'
-};
-
-export default config;
